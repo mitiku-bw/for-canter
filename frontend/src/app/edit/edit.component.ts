530972e7f0492efd06190fd933be22c22e1a936f
mitiku-bw <35116147+mitiku-bw@users.noreply.github.com>
2018-07-05 07:10:09 +0300

    Update edit.component.ts

--- frontend/src/app/edit/edit.component.ts
+++ frontend/src/app/edit/edit.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-edit',
-  templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  templateUrl: './edit.component.html'
 })
 export class EditComponent implements OnInit {
 
