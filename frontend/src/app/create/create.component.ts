b82d8771b5bec899f35ba0844a54d86cac3f27df
mitiku-bw <35116147+mitiku-bw@users.noreply.github.com>
2018-07-05 07:10:36 +0300

    Update create.component.ts

--- frontend/src/app/create/create.component.ts
+++ frontend/src/app/create/create.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  templateUrl: './create.component.html'
 })
 export class CreateComponent implements OnInit {
 
